774df72d622c889ba3f78e2dcd1ea1a600ac07d5
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-05 16:52:50 +0200

    About style

--- src/Components/3_about/About.jsx
+++ src/Components/3_about/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./style.css";
 
 function About() {
     return (
         <>
-            <h1>about</h1>
+            <div className='about py-md-5'>
+                <div className="container py-md-5">
+                    <div className="d-flex flex-column gap-3 align-items-center py-5 bg-transparent text-white text-center">
+                        <h1 className='text-uppercase text-center'>about component</h1>
+                        <span className='icon position-relative m-auto' style={{ width: '4rem' }}><i className="bi bi-star-fill"/></span>
+                        <div className="d-flex flex-column flex-md-row w-75 text-start">
+                            <p className='d-block'>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization. </p>
+                            <p className='d-block'>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
