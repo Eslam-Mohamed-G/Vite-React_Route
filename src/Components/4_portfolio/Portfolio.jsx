d09475c94d167f3781f2a37c41f22ccdd9edee64
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-06 11:09:05 +0200

    portfolio style

--- src/Components/4_portfolio/Portfolio.jsx
+++ src/Components/4_portfolio/Portfolio.jsx
@@ -12,40 +12,59 @@ function Portfolio() {
                     <h1 className='text-uppercase text-center'>portfolio component</h1>
                     <span className='icon position-relative m-auto' style={{ width: '4rem' }}><i className="bi bi-star-fill" /></span>
                 </div>
+                
                 <div className="row g-5 my-0">
                     <div className='col-lg-4 col-md-6'>
-                        <div className='content rounded overflow-hidden'>
+                        <div className='content rounded overflow-hidden position-relative'>
                             <img className='w-100' src={poert1} alt="" />
+                            <div className='bg-black position-absolute top-0 bottom-0 start-0 end-0 align-content-center text-center'>
+                                <span><i className="bi bi-plus text-white" /></span>
+                            </div>
                         </div>
                     </div>
 
                     <div className='col-lg-4 col-md-6'>
-                        <div className='content rounded overflow-hidden'>
+                        <div className='content rounded overflow-hidden position-relative'>
                             <img className='w-100' src={port2} alt="" />
+                            <div className='bg-black position-absolute top-0 bottom-0 start-0 end-0 align-content-center text-center'>
+                                <span><i className="bi bi-plus text-white" /></span>
+                            </div>
                         </div>
                     </div>
 
                     <div className='col-lg-4 col-md-6'>
-                        <div className='content rounded overflow-hidden'>
+                        <div className='content rounded overflow-hidden position-relative'>
                             <img className='w-100' src={port3} alt="" />
+                            <div className='bg-black position-absolute top-0 bottom-0 start-0 end-0 align-content-center text-center'>
+                                <span><i className="bi bi-plus text-white" /></span>
+                            </div>
                         </div>
                     </div>
 
                     <div className='col-lg-4 col-md-6'>
-                        <div className='content rounded overflow-hidden'>
+                        <div className='content rounded overflow-hidden position-relative'>
                             <img className='w-100' src={poert1} alt="" />
+                            <div className='bg-black position-absolute top-0 bottom-0 start-0 end-0 align-content-center text-center'>
+                                <span><i className="bi bi-plus text-white" /></span>
+                            </div>
                         </div>
                     </div>
 
                     <div className='col-lg-4 col-md-6'>
-                        <div className='content rounded overflow-hidden'>
+                        <div className='content rounded overflow-hidden position-relative'>
                             <img className='w-100' src={port2} alt="" />
+                            <div className='bg-black position-absolute top-0 bottom-0 start-0 end-0 align-content-center text-center'>
+                                <span><i className="bi bi-plus text-white" /></span>
+                            </div>
                         </div>
                     </div>
 
                     <div className='col-lg-4 col-md-6'>
-                        <div className='content rounded overflow-hidden'>
+                        <div className='content rounded overflow-hidden position-relative'>
                             <img className='w-100' src={port3} alt="" />
+                            <div className='bg-black position-absolute top-0 bottom-0 start-0 end-0 align-content-center text-center'>
+                                <span><i className="bi bi-plus text-white" /></span>
+                            </div>
                         </div>
                     </div>
                 </div>
